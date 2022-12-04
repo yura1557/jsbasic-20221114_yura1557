73bb46fa95a555664c1afd62496d0336b4ead2ef
Iurii <yura1557@gmail.com>
2022-12-04 21:38:00 +0300

    внес все правки по замечаниям и сделал 4й модуль

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  for (let item of friends) {
+      let li = document.createElement('li');
+      li.innerHTML = `${item.firstName} ${item.lastName}`;
+      ul.appendChild(li);
+  }
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,16 @@
 function highlight(table) {
-  // ваш код...
+  tbody = table.tBodies;
+  for (let row of table.tBodies[0].rows) {
+
+    let age = row.cells[1].innerHTML
+		if (age < 18) row.style.textDecoration = 'line-through'
+
+		let gender = row.cells[2].innerHTML
+		if (gender == 'm') row.classList.add('male')
+		if (gender == 'f') row.classList.add('female')
+
+		let available = row.cells[3].dataset.available
+		if (available) row.classList.add(available == 'true' ? 'available' : 'unavailable')
+		else row.hidden = true  
+  }
 }
